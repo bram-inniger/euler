2aa1d998c50b1aa78e73fab0fc480b1106e52fcc
Bram Inniger <braminniger@gmail.com>
2016-06-18 01:01:17 +0300

    Added problem loading mechanism

--- src/main/java/be/inniger/euler/Main.java
+++ src/main/java/be/inniger/euler/Main.java
@@ -16,13 +16,21 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
+
 package be.inniger.euler;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import com.google.common.reflect.ClassPath;
 
 /**
- * Main class as entry point for the JAR
+ * Main class of the project, invoke to solve a specific problem by problem nr.
  *
  * @author Bram Inniger
  * @version 1.0
@@ -30,14 +38,79 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 
   private static final Logger log = LogManager.getLogger(Main.class);
+  private static final int FAIL_CODE = 1;
 
   /**
-   * Main entry point
-   * @param args Optional arguments
+   * Main entry point.
+   *
+   * @param args A single integer, signifying the problem to be solved
    */
-  public static void main(String... args) {
-    int answer = 42;
-    log.info("{}", answer);
+  public static void main(@NotNull String... args) throws Exception {
+    try {
+      Map<String, ClassPath.ClassInfo> availableProblems = getAvailableProblems();
+      int problemNr = getProblemNumber(args);
+      String problemName = "Problem" + (problemNr < 10 ? "0" : "") + problemNr;
+
+      if (!availableProblems.containsKey(problemName)) {
+        throw new IllegalArgumentException("Problem not found: " + problemName);
+      }
+
+      Class<?> problemClass = Class.forName(availableProblems.get(problemName).getName());
+      Problem problem = (Problem) problemClass.newInstance();
+
+      log.info("The solution is: \"{}\"", problem.solve());
+    }
+    catch (IllegalArgumentException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+      log.error("Caught \"{}\" with message \"{}\"", e.getClass(), e.getMessage());
+      exitFail();
+    }
+  }
+
+  /**
+   * Get all of the available problems to solve.
+   *
+   * @return A Map of all available problems, with their single classname as the key
+   */
+  @NotNull
+  private static Map<String, ClassPath.ClassInfo> getAvailableProblems() {
+    try {
+      ClassLoader loader = Thread.currentThread().getContextClassLoader();
+      return ClassPath.from(loader).getTopLevelClasses()
+          .stream()
+          .filter(info -> info.getName().startsWith("be.inniger.euler.problems"))
+          .collect(Collectors.toMap(info -> {
+            String fullName = info.getName();
+            return fullName.substring(fullName.lastIndexOf('.') + 1);
+          }, Function.identity()));
+    }
+    catch (IOException e) {
+      log.error("Failed to retrieve the list of problems: \"{}\"", e.getMessage());
+      exitFail();
+    }
+
+    return Collections.emptyMap(); // Will not be reached anyway
+  }
+
+  /**
+   * Validate the inputs and extract the problem nr.
+   *
+   * @param args A single integer, signifying the problem to be solved
+   * @return The number of the to be solved problem
+   * @throws IllegalArgumentException The input should be a single integer
+   */
+  private static int getProblemNumber(@NotNull String... args) throws IllegalArgumentException {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Expected 1 and exactly 1 argument! ");
+    }
+
+    return Integer.parseInt(args[0]);
+  }
+
+  /**
+   * Exit the JVM with the fail error code, something went horrible wrong!
+   */
+  private static void exitFail() {
+    log.error("Exiting now...");
+    System.exit(FAIL_CODE);
   }
 }
-
