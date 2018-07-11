82fc29ae91c6d027bee9a8b8028605bb2b1ab6c3
Bram Inniger <bram@inniger.be>
2018-07-11 23:55:05 +0300

    Cleaned up Problem02

--- src/test/java/be/inniger/euler/problems01to10/Problem01Test.java
+++ src/test/java/be/inniger/euler/problems01to10/Problem01Test.java
@@ -1,22 +1,3 @@
-/*
- * Project Euler solution repository
- * Copyright (C) 2016 Bram Inniger
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
- */
-
 package be.inniger.euler.problems01to10;
 
 import org.junit.Test;
@@ -27,9 +8,6 @@ public class Problem01Test {
 
   @Test
   public void testSolve() {
-    int expected = 233168;
-    int actual = new Problem01().solve();
-
-    assertEquals(expected, actual);
+    assertEquals(233168, new Problem01().solve());
   }
 }
